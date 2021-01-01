5bb64b9d1047bd0ea36fb3970b915526142b5ec9
Juan Jacobs <juan@producteca.com>
2021-01-01 14:27:22 -0600

    refactor dumpReader

--- src/domain/dumpReader.js
+++ src/domain/dumpReader.js
@@ -16,19 +16,53 @@ module.exports = class DumpReader {
   }
 
   methodHook({ className, name, mods }) {
-    const classDefinition = `public class ${className} `;
-    const classIndex = _.findIndex(this.dumpLines, it => _.includes(it, classDefinition));
-    console.log(`Found class ${className} in line ${classIndex + 1}`);
-    const classBody = this.dump.split(classDefinition)[1];
-    const lines = classBody.split("\n");
-    const relativeMethodIndex = _.findIndex(lines, it => _.includes(it, `${name}(`)); //ex index for GetTruePosition(. Line above is RVA: 0x8E6360 Offset: 0x8E4B60 VA: 0x108E6360
-    const relativeRvaIndex = relativeMethodIndex - 1;
-    const rva = lines[relativeRvaIndex].split(":")[1].split(" ")[1].trim();
-    console.log(`Found method ${name} in line ${classIndex + relativeMethodIndex + 1}. RVA: ${rva}`);
-    return { className, name, mods, rva };
+    const classIndex = this._findClassIndex(className);
+    const { index, line, classLines } = this._findMethodInClass(className, name);
+    const relativeRvaIndex = index - 1;
+    const rva = classLines[relativeRvaIndex].split(":")[1].split(" ")[1].trim();
+    console.log(`Found method ${line} in line ${classIndex + index + 1}. RVA: ${rva}`);
+    return { className, name, mods, rva, classIndex, relativeRvaIndex };
   }
 
-  pathHook({ name, entry }) {
+  pathHook({ name, entry, path }) {
+    const properties = path.split(".");
+    const offsets = properties.map(property => this._propertyOffset({name, entry, path}, property));
+    return { name, entry, path, offsets };
+  }
 
+  _propertyOffset() {
+    return {} //TODO
+  }
+
+  _findClassIndex(className) {
+    const classDefinition = this._classDefinition(className);
+    const classIndex = _.findIndex(this.dumpLines, it => _.includes(it, classDefinition));
+    console.log(`Found class ${className} in line ${classIndex + 1}`)
+    return classIndex;
+  }
+
+  _findFieldInClass(className, field) {
+    return this._findInClass(className, `${field};`);
+  }
+  
+  _findMethodInClass(className, method) {
+    return this._findInClass(className, `${method}(`);
+  }
+
+  _findInClass(className, search) {
+    const classLines = this._classLines(className);
+    const index = _.findIndex(classLines, it => _.includes(it, search));
+    return { index, line: classLines[index], classLines };
+  }
+
+  _classLines(className) {
+    const classDefinition = this._classDefinition(className);
+    const classBody = this.dump.split(classDefinition)[1];
+    const lines = classBody.split("\n");
+    return lines;
+  }
+
+  _classDefinition(className) {
+    return `public class ${className} `;
   }
 }
