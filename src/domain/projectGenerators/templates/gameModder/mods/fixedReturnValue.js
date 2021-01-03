f32b7e24eaa606b8fa80a6b742851ba77d1678aa
Juan Jacobs <juan@producteca.com>
2021-01-03 16:47:49 -0600

    add functionName for consistency

--- src/domain/projectGenerators/templates/gameModder/mods/fixedReturnValue.js
+++ src/domain/projectGenerators/templates/gameModder/mods/fixedReturnValue.js
@@ -1,6 +1,6 @@
 const { buildHook } = require("./hookUtils");
 
-const fixedReturnValue = (options, mod) => {
+const fixedReturnValue = (options, mod, functionName) => {
     const { name } = options;
     const hackedBody = () => `return ${mod.args};`;
     const definition =  buildHook(options, hackedBody);
--- src/domain/projectGenerators/templates/gameModder/mods/replaceImplementation.js
+++ src/domain/projectGenerators/templates/gameModder/mods/replaceImplementation.js
@@ -1,6 +1,6 @@
 const { buildHook } = require("./hookUtils");
 
-const replaceImplementation = (options, mod) => {
+const replaceImplementation = (options, mod, functionName) => {
     const { name } = options;
     const hackedBody = () => mod.args;
     const definition = buildHook(options, hackedBody);
