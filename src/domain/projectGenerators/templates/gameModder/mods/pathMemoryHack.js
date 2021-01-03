e9b7f44e29c73f8ffd784c02ab35236f14a9c7c2
Juan Jacobs <juan@producteca.com>
2021-01-03 04:02:35 -0600

    fix this saving

--- src/domain/projectGenerators/templates/gameModder/mods/pathMemoryHack.js
+++ src/domain/projectGenerators/templates/gameModder/mods/pathMemoryHack.js
@@ -3,11 +3,13 @@ const { buildHook, hookDataThis } = require("./hookUtils");
 const pathMemoryHack = (options, mod) => {
     const { className, name, rva } = options;
     const property = hookDataThis(options);
-    const hackedBody = () => `uintptr_t ${name}This = (uintptr_t)thisReference;
-    if ((*myHookedData).${property} != thisReference) 
+    const thisName = `${name}This`;
+    const hackedBody = () => `
+    uintptr_t ${thisName} = (uintptr_t)thisReference;
+    if ((*myHookedData).${property} != ${thisName}) 
     {
-        printf("Reassigning ${property} from %x to %x\\n", (*myHookedData).${property}, thisReference);
-        (*myHookedData).${property} = thisReference;
+        printf("Reassigning ${property} from %x to %x\\n", (*myHookedData).${property}, ${thisName});
+        (*myHookedData).${property} = ${thisName};
     }
     return original${name}(thisReference);`;
 
