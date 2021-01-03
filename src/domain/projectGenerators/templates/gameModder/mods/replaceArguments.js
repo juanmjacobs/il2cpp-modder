457240d3ce111348525a23df24f39c49a14571a3
Juan Jacobs <juan@producteca.com>
2021-01-03 16:45:43 -0600

    fix function hook renaming

--- src/domain/projectGenerators/templates/gameModder/mods/replaceArguments.js
+++ src/domain/projectGenerators/templates/gameModder/mods/replaceArguments.js
@@ -1,8 +1,8 @@
 const { buildHook } = require("./hookUtils");
 
-const replaceArguments = (options, mod) => {
+const replaceArguments = (options, mod, functionName) => {
     const { name } = options;
-    const hackedBody = () => `return original${name}(thisReference, ${mod.args.join(", ")});`
+    const hackedBody = () => `return original_${functionName}(thisReference, ${mod.args.join(", ")});`
     const definition =  buildHook(options, hackedBody);
     return definition;
 }
--- src/domain/projectGenerators/templates/gameModder/mods/replaceImplementation.js
+++ src/domain/projectGenerators/templates/gameModder/mods/replaceImplementation.js
@@ -2,8 +2,8 @@ const { buildHook } = require("./hookUtils");
 
 const replaceImplementation = (options, mod) => {
     const { name } = options;
-    const hackedBody = mod.args;
-    const definition = buildHook(options, args);
+    const hackedBody = () => mod.args;
+    const definition = buildHook(options, hackedBody);
     return definition;
 }
 
