584555a14def5e556ee3a507caf42992c0995d44
Juan Jacobs <juan@producteca.com>
2021-01-03 18:16:06 -0600

    chooseFolder sample bat

---
+++ chooseFolder.bat
@@ -0,0 +1,15 @@
+:: fchooser.bat
+:: launches a folder chooser and outputs choice to the console
+:: https://stackoverflow.com/a/15885133/1683264
+
+@echo off
+setlocal
+
+set "psCommand="(new-object -COM 'Shell.Application')^
+.BrowseForFolder(0,'Select your game directory',0,0).self.path""
+
+for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "folder=%%I"
+
+setlocal enabledelayedexpansion
+echo You chose !folder!
+endlocal
